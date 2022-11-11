98dc9cd87f7baa9b93f1ef0c56c7a131eeabadb1
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2022-11-11 13:50:33 +0530

    Theme Added

---
+++ pages/calculator/calculator.js
@@ -0,0 +1,28 @@
+
+function pressButton(buttonValue) {
+    let calText = document.querySelector(".input-screen").value + buttonValue + "";
+    document.querySelector(".input-screen").value = calText;
+}
+
+function calculate() {
+    let calText = document.querySelector(".input-screen").value;
+    document.querySelector(".input-screen").value = eval(calText);
+}
+
+function clearScreen() {
+    document.querySelector(".input-screen").value = "";
+}
+
+function backBtn() {
+    let back = document.querySelector(".input-screen").value;
+    document.querySelector(".input-screen").value = back.substring(0, back.length - 1);
+}
+
+function on() {
+
+}
+
+function off() {
+    document.querySelector("input").disabled;
+}
+
